e6d0e73f177101594cb2e34d60b397abead73e7b
Julian Hirt <julian@couchbits.com>
2020-04-14 13:27:06 +0200

    Use logger instead of print statements

--- RFunction.R
+++ RFunction.R
@@ -11,35 +11,35 @@ rFunction = function(username, password, study, animals, timestamp_start=NULL, t
   arguments[["deploymentAsIndividuals"]] = FALSE
 
   if (exists("timestamp_start")&& !is.null(timestamp_start)) {
-    print(paste0("timestamp_start is set and will be used: ", timestamp_start))
+    logger.info(paste0("timestamp_start is set and will be used: ", timestamp_start))
     arguments["timestamp_start"] = timestamp_start
   }else {
-    print("timestamp_start not set.")
+    logger.info("timestamp_start not set.")
   }
 
   if (exists("timestamp_end") && !is.null(timestamp_end)) {
-    print(paste0("timestamp_end is set and will be used: ", timestamp_end))
+    logger.info(paste0("timestamp_end is set and will be used: ", timestamp_end))
     arguments["timestamp_end"] = timestamp_end
   }else {
-    print("timestamp_end not set.")
+    logger.info("timestamp_end not set.")
   }
 
   if(length(animals) != 0) {
-    print(paste0(length(animals), " animals: ", animals))
+    logger.info(paste0(length(animals), " animals: ", animals))
     all <- foreach(animal = animals) %do% {
       arguments["animalName"] = animal
-        print(animal)
+        logger.info(animal)
       do.call(getMovebankData, arguments)
     }
     result = moveStack(all)
   } else {
-    print("no animals set, using full study")
+    logger.info("no animals set, using full study")
 
     result <- do.call(getMovebankData, arguments)
   }
 
   if (exists("data") && !is.null(data)) {
-    print("Merging input and result together")
+    logger.info("Merging input and result together")
     result <- moveStack(result, data)
   }
   result
