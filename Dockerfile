4fe5c046e9f26c51c330f948ddea94dba3a51a25
Julian Hirt <julian@couchbits.com>
2020-07-02 13:29:11 +0200

    Make sure result is always a moveStack

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM registry.gitlab.com/couchbits/movestore/movestore-groundcontrol/movestore-apps/pilot-base
-
-# Install R dependencies
-RUN R -e "install.packages(c('foreach'), repos='https://cloud.r-project.org/')"
-
-COPY RFunction.R /root/app/r/RFunction.R
--- RFunction.R
+++ RFunction.R
@@ -1,46 +1,51 @@
 library('move')
 library('foreach')
 
-rFunction = function(username, password, study, animals, timestamp_start=NULL, timestamp_end=NULL, data=NULL) {
+rFunction  <- function(username, password, study, animals, timestamp_start=NULL, timestamp_end=NULL, data=NULL) {
   credentials <- movebankLogin(username, password)
   arguments <- list()
   arguments[["study"]] = study
   arguments[["login"]] = credentials
-  arguments[["removeDuplicatedTimestamps"]] = FALSE
+  arguments[["removeDuplicatedTimestamps"]] = TRUE
   arguments[["includeExtraSensors"]] = FALSE
   arguments[["deploymentAsIndividuals"]] = FALSE
-
+  
   if (exists("timestamp_start")&& !is.null(timestamp_start)) {
-    logger.info(paste0("timestamp_start is set and will be used: ", timestamp_start))
+    print(paste0("timestamp_start is set and will be used: ", timestamp_start))
     arguments["timestamp_start"] = timestamp_start
   }else {
-    logger.info("timestamp_start not set.")
+    print("timestamp_start not set.")
   }
-
+  
   if (exists("timestamp_end") && !is.null(timestamp_end)) {
-    logger.info(paste0("timestamp_end is set and will be used: ", timestamp_end))
+    print(paste0("timestamp_end is set and will be used: ", timestamp_end))
     arguments["timestamp_end"] = timestamp_end
   }else {
-    logger.info("timestamp_end not set.")
+    print("timestamp_end not set.")
   }
-
+  
   if(length(animals) != 0) {
-    logger.info(paste0(length(animals), " animals: ", animals))
+    print(paste0(length(animals), " animals: ", animals))
     all <- foreach(animal = animals) %do% {
       arguments["animalName"] = animal
-        logger.info(animal)
+      print(animal)
       do.call(getMovebankData, arguments)
     }
-    result = moveStack(all)
+    result <- moveStack(all)
   } else {
-    logger.info("no animals set, using full study")
-
+    print("no animals set, using full study")
     result <- do.call(getMovebankData, arguments)
+  }
+  
+  # Fallback to make sure it is always a moveStack object and not a move object.
+  if (is.(result,'Move')) {
+    result <- moveStack(result)
   }
 
   if (exists("data") && !is.null(data)) {
-    logger.info("Merging input and result together")
+    print("Merging input and result together")
     result <- moveStack(result, data)
   }
   result
 }
+
