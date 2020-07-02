e128165fe4ca22f550647d31975563492121b488
Julian Hirt <julian@couchbits.com>
2020-07-02 14:12:57 +0200

    Undo removing duplicates

--- RFunction.R
+++ RFunction.R
@@ -6,24 +6,24 @@ rFunction  <- function(username, password, study, animals, timestamp_start=NULL,
   arguments <- list()
   arguments[["study"]] = study
   arguments[["login"]] = credentials
-  arguments[["removeDuplicatedTimestamps"]] = TRUE
+  arguments[["removeDuplicatedTimestamps"]] = FALSE
   arguments[["includeExtraSensors"]] = FALSE
   arguments[["deploymentAsIndividuals"]] = FALSE
-  
+
   if (exists("timestamp_start")&& !is.null(timestamp_start)) {
     print(paste0("timestamp_start is set and will be used: ", timestamp_start))
     arguments["timestamp_start"] = timestamp_start
   }else {
     print("timestamp_start not set.")
   }
-  
+
   if (exists("timestamp_end") && !is.null(timestamp_end)) {
     print(paste0("timestamp_end is set and will be used: ", timestamp_end))
     arguments["timestamp_end"] = timestamp_end
   }else {
     print("timestamp_end not set.")
   }
-  
+
   if(length(animals) != 0) {
     print(paste0(length(animals), " animals: ", animals))
     all <- foreach(animal = animals) %do% {
@@ -36,7 +36,7 @@ rFunction  <- function(username, password, study, animals, timestamp_start=NULL,
     print("no animals set, using full study")
     result <- do.call(getMovebankData, arguments)
   }
-  
+
   # Fallback to make sure it is always a moveStack object and not a move object.
   if (is(result,'Move')) {
     result <- moveStack(result)
@@ -48,4 +48,3 @@ rFunction  <- function(username, password, study, animals, timestamp_start=NULL,
   }
   result
 }
-
